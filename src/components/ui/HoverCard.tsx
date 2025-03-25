c1a1e8e1cf216f9cc3a1c6d281e96dcf1996d890
Isserh <iss.serhani@gmail.com>
2025-03-25 20:22:07 +0000

    inclinaison cartes

--- src/components/ui/HoverCard.tsx
+++ src/components/ui/HoverCard.tsx
@@ -10,8 +10,8 @@ export const HoverCard: React.FC<HoverCardProps> = ({ children, className = "" }
   const x = useMotionValue(0);
   const y = useMotionValue(0);
 
-  const rotateX = useTransform(y, [-100, 100], [30, -30]);
-  const rotateY = useTransform(x, [-100, 100], [-30, 30]);
+  const rotateX = useTransform(y, [-30, 30], [3, -3]);
+  const rotateY = useTransform(x, [-30, 30], [-3, 3]);
 
   const springConfig = { damping: 15, stiffness: 150 };
   const springRotateX = useSpring(rotateX, springConfig);
@@ -19,8 +19,8 @@ export const HoverCard: React.FC<HoverCardProps> = ({ children, className = "" }
 
   const handleMouseMove = (event: React.MouseEvent<HTMLDivElement>) => {
     const rect = event.currentTarget.getBoundingClientRect();
-    const centerX = rect.left + rect.width / 2;
-    const centerY = rect.top + rect.height / 2;
+    const centerX = rect.left + rect.width / 5;
+    const centerY = rect.top + rect.height / 5;
     
     x.set(event.clientX - centerX);
     y.set(event.clientY - centerY);
